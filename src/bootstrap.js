35ed055c8e3f674ade3e37822e5f31bb4745c62f
Brian Baliach <brianbaliach@Brians-MacBook-Pro.local>
2022-07-13 21:02:30 +0300

    [BUG]:Fixed re-rendering issue

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,22 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
+let root
 const mount = (el) => {
-    console.log("Ahem!!!!");
-    const root = ReactDOM.createRoot(el);
+    if (!root) {
+        root = ReactDOM.createRoot(el);
 
-    root.render(
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>
-    );
+        root.render(
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>
+        );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-    reportWebVitals();
+        reportWebVitals();
+    }
 }
+
 // We do this in case we want to run this project in conjunction with another project that depends on this one
 // But we also want to test our changes directly within this project. See, the mount function will always be called when
 // this file is imported.
